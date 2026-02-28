cdd9c6b8dfe2b5613b827dcf4e28bfb23c24afd1
root <root@ip-10-0-14-102.ap-south-2.compute.internal>
2026-02-28 15:17:48 +0000

    Changed the desired count

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,6 @@ variable "aws_region" { default = "ap-south-2" }
 variable "app_name" { default = "cicd-demo-app" }
 variable "ecr_image_uri" { type = string } # Passed by Jenkins at runtime
 variable "container_port" { default = 3000 }
-variable "desired_count" { default = 2 }
+variable "desired_count" { default = 1 }
 variable "task_cpu" { default = "256" } # 0.25 vCPU
 variable "task_memory" { default = "512" } # 512 MiB
